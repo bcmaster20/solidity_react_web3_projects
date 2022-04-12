b7a0ea68d31e56bce91253113a0dedb42c73678e
Jaylee <syuueih@gmail.com>
2022-04-11 20:18:30 -0700

    get object vending machine contract
    Signed-off-by: Jaylee <syuueih@gmail.com>

--- 03_truffle_ganache/readme.txt
+++ 03_truffle_ganache/readme.txt
@@ -22,7 +22,25 @@ Truffle Dapp intergrate our UI
 $ npx create-next-app vending-machine-app
 $ npm i bulma
 $ npm i web3
+    
+● package.json
+"compile": "solcjs --abi --include-path node_modules/ --base-path . blockchain/contracts/VendingMachine.sol --output-dir blockchain/build"
 
+● add module in package.json 
+"solc": "^0.8.11",
+
+
+● How to get object of smartcontract deployed test network?
+import Web3 from 'web3'
+const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/75c8616a13e942e08f55951b33d8c1a6');
+const web3 = new Web3(provider);
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+const vendingMachineContract = new web3.eth.Contract(abi, "0xCd44c67F8f8B0F10D2A955F5B6B5E6Cc36437E07");
+export default vendingMachineContract
+
+
+
+https://rinkeby.infura.io/v3/75c8616a13e942e08f55951b33d8c1a6
 ● Reference Site
 ・web3
 https://web3js.readthedocs.io/en/v1.7.1/ssss
--- 03_truffle_ganache/vendingmachine-dapp/blockchain/vending.js
+++ 03_truffle_ganache/vendingmachine-dapp/blockchain/vending.js
@@ -1,13 +1,25 @@
 
 
 // abi copy / pasted from './build/blockchain_contracts_VendingMachine_sol_VendingMachine.abi'
-const vendingMachineABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+// const vendingMachineABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
 
-const vendingMachineContract = (web3) => {
-  return new web3.eth.Contract(
-    vendingMachineABI,
-    "0xCd44c67F8f8B0F10D2A955F5B6B5E6Cc36437E07"
-  )
-}
+// const vendingMachineContract = (web3) => {
+//   return new web3.eth.Contract(
+//     vendingMachineABI,
+//     "0xCd44c67F8f8B0F10D2A955F5B6B5E6Cc36437E07"
+//   )
+// }
+
+// export default vendingMachineContract
+
+import Web3 from 'web3'
+
+const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/75c8616a13e942e08f55951b33d8c1a6');
+
+const web3 = new Web3(provider);
+
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+
+const vendingMachineContract = new web3.eth.Contract(abi, "0xCd44c67F8f8B0F10D2A955F5B6B5E6Cc36437E07");
 
 export default vendingMachineContract
